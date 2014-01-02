b3f06307b24828bbd95afb9659590082239d88bb
Tony Han <h.bing612@gmail.com>
2014-01-02 23:43:57 +0800

    add spec for TryRb::CLI::fullpath

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -14,6 +14,44 @@ describe TryRb::CLI do
       TryRb::CLI.start(["filename"])
     end
   end
+
+  describe '::fullpath' do
+    before :each do
+      TryRb::Config.instance.path = fixture_file_path('tryrbrc')
+      FileUtils.mkdir project_path + '/tmp' unless Dir.exists?(project_path + '/tmp')
+    end
+    after :each do
+      FileUtils.rmdir project_path + '/tmp'
+    end
+    it 'return fullpath of the file' do
+      expect(TryRb::CLI.fullpath('bar')).to eq('~/tmp/foo/tryrb/bar.rb')
+    end
+    it 'return right filename with a file *.rb' do
+      expect(TryRb::CLI.fullpath('bar.rb')).to eq('~/tmp/foo/tryrb/bar.rb')
+    end
+    context 'when the Dir does not exists' do
+      it 'create a dir' do
+        Dir.stub(:exists?) { false }
+        TryRb::Config.instance.stub(:tmp_dir) { project_path + '/tmp/tryrb' }
+        expect(FileUtils).to receive(:mkdir_p).with(project_path + '/tmp/tryrb')
+        TryRb::CLI.fullpath('bar')
+      end
+    end
+    context 'when the Dir exists' do
+      it 'does not create a dir' do
+        Dir.stub(:exists?) { true }
+        TryRb::Config.instance.stub(:tmp_dir) { project_path + '/tmp/tryrb' }
+        expect(FileUtils).to_not receive(:mkdir_p).with(project_path + '/tmp/tryrb')
+        TryRb::CLI.fullpath('bar')
+      end
+    end
+    context 'when there is a file which has same name with dir' do
+      it 'aborts' do
+        TryRb::Config.instance.stub(:tmp_dir) { project_path + '/tmp/foo/tryrb' }
+        FileUtils.touch project_path + '/tmp/foo'
+        expect(TryRb::CLI).to receive(:abort).with(/File .+ exists. The tmp directory can't be created! Please delete the file first./)
+        TryRb::CLI.fullpath('bar')
+      end
     end
   end
 end
--- spec/helper.rb
+++ spec/helper.rb
@@ -14,3 +14,7 @@ end
 def fixture_file_path(file_name)
   File.join fixture_path, file_name
 end
+
+def project_path
+  File.expand_path('../..', __FILE__)
+end
