0ef725367dfbe1853ab617b44f1c39a840102d44
Tony Han <h.bing612@gmail.com>
2014-01-03 01:02:23 +0800

    let test pass

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -7,13 +7,13 @@ describe TryRb::CLI do
   describe '::start' do
     it "should call system method with an editor" do
       TryRb::Config.instance.path = fixture_file_path('tryrbrc')
-      expect(TryRb::CLI).to receive(:system).with('emacs', '~/tmp/foo/tryrb/20140101000000_filename.rb')
+      expect(TryRb::CLI).to receive(:system).with('emacs ~/tmp/foo/tryrb/20140101000000_filename.rb')
       TryRb::CLI.start(["filename"])
     end
 
     it "should call system method with right filepath and default editor" do
       TryRb::Config.instance.path = fixture_file_path('foorc')
-      expect(TryRb::CLI).to receive(:system).with('mvim', '~/tmp/tryrb/20140101000000_filename.rb')
+      expect(TryRb::CLI).to receive(:system).with('mvim ~/tmp/tryrb/20140101000000_filename.rb')
       TryRb::CLI.start(["filename"])
     end
   end
