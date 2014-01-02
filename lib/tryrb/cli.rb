4bc01eff893323f56b0750861d923c9efbee64fb
Tony Han <h.bing612@gmail.com>
2014-01-03 00:24:27 +0800

    add now time prefix to the filename

--- lib/tryrb/cli.rb
+++ lib/tryrb/cli.rb
@@ -11,12 +11,26 @@ module TryRb
       end
 
       def fullpath(filename)
-        filename = filename.gsub(/\.rb$/, '') + '.rb'
+        filename = get_filename(filename)
         expanded_path = File.expand_path(@config.tmp_dir)
         FileUtils.mkdir_p expanded_path unless Dir.exists?(expanded_path)
         File.join @config.tmp_dir, filename
       rescue Errno::EEXIST => e
         abort "File #{e.message.split[-1]} exists. The tmp directory can't be created! Please delete the file first."
+      end
+
+      def filename_prefix
+        Time.now.strftime("%Y%m%d%H%M%S")
+      end
+
+      def filename_extname
+        '.rb'
+      end
+
+      def get_filename(name)
+        parts = [filename_prefix]
+        parts << name.gsub(/\.rb$/, '') if name
+        parts * '_' + filename_extname
       end
     end
   end
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,16 +1,19 @@
 require 'helper'
 
 describe TryRb::CLI do
+  before :each do
+    Time.stub(:now) { Date.parse('2014-1-1').to_time }
+  end
   describe '::start' do
     it "should call system method with an editor" do
       TryRb::Config.instance.path = fixture_file_path('tryrbrc')
-      expect(TryRb::CLI).to receive(:system).with('emacs', '~/tmp/foo/tryrb/filename.rb')
+      expect(TryRb::CLI).to receive(:system).with('emacs', '~/tmp/foo/tryrb/20140101000000_filename.rb')
       TryRb::CLI.start(["filename"])
     end
 
     it "should call system method with right filepath and default editor" do
       TryRb::Config.instance.path = fixture_file_path('foorc')
-      expect(TryRb::CLI).to receive(:system).with('mvim', '~/tmp/tryrb/filename.rb')
+      expect(TryRb::CLI).to receive(:system).with('mvim', '~/tmp/tryrb/20140101000000_filename.rb')
       TryRb::CLI.start(["filename"])
     end
   end
@@ -24,11 +27,12 @@ describe TryRb::CLI do
       FileUtils.rmdir project_path + '/tmp'
     end
     it 'return fullpath of the file' do
-      expect(TryRb::CLI.fullpath('bar')).to eq('~/tmp/foo/tryrb/bar.rb')
+      expect(TryRb::CLI.fullpath('bar')).to eq('~/tmp/foo/tryrb/20140101000000_bar.rb')
     end
     it 'return right filename with a file *.rb' do
-      expect(TryRb::CLI.fullpath('bar.rb')).to eq('~/tmp/foo/tryrb/bar.rb')
+      expect(TryRb::CLI.fullpath('bar.rb')).to eq('~/tmp/foo/tryrb/20140101000000_bar.rb')
     end
+
     context 'when the Dir does not exists' do
       it 'create a dir' do
         Dir.stub(:exists?) { false }
@@ -37,6 +41,7 @@ describe TryRb::CLI do
         TryRb::CLI.fullpath('bar')
       end
     end
+
     context 'when the Dir exists' do
       it 'does not create a dir' do
         Dir.stub(:exists?) { true }
@@ -45,12 +50,32 @@ describe TryRb::CLI do
         TryRb::CLI.fullpath('bar')
       end
     end
+
     context 'when there is a file which has same name with dir' do
       it 'aborts' do
         TryRb::Config.instance.stub(:tmp_dir) { project_path + '/tmp/foo/tryrb' }
         FileUtils.touch project_path + '/tmp/foo'
         expect(TryRb::CLI).to receive(:abort).with(/File .+ exists. The tmp directory can't be created! Please delete the file first./)
         TryRb::CLI.fullpath('bar')
+      end
+    end
+  end
+
+  describe '#filename_prefix' do
+    it "return now string" do
+      expect(TryRb::CLI.filename_prefix).to eq("20140101000000")
+    end
+  end
+
+  describe '#get_filename' do
+    context 'when name is given' do
+      it 'returns string containing the name' do
+        expect(TryRb::CLI.get_filename('bar')).to eq('20140101000000_bar.rb')
+      end
+    end
+    context 'when name is not given' do
+      it 'returns just time string' do
+        expect(TryRb::CLI.get_filename(nil)).to eq('20140101000000.rb')
       end
     end
   end
