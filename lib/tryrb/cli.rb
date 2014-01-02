a31d81e61691864b138344c048f9bde3e80c2269
Tony Han <h.bing612@gmail.com>
2014-01-02 23:42:22 +0800

    use config to run system command

--- lib/tryrb/cli.rb
+++ lib/tryrb/cli.rb
@@ -1,12 +1,22 @@
 require 'tryrb/config'
+require 'fileutils'
 
 module TryRb
   class CLI
-    @editor = "mvim"
+    @config = TryRb::Config.instance
     class << self
-      attr_accessor :editor
+      attr_writer :config
       def start(args)
-        system(@editor, args[0])
+        system(@config.editor, fullpath(args[0]))
+      end
+
+      def fullpath(filename)
+        filename = filename.gsub(/\.rb$/, '') + '.rb'
+        expanded_path = File.expand_path(@config.tmp_dir)
+        FileUtils.mkdir_p expanded_path unless Dir.exists?(expanded_path)
+        File.join @config.tmp_dir, filename
+      rescue Errno::EEXIST => e
+        abort "File #{e.message.split[-1]} exists. The tmp directory can't be created! Please delete the file first."
       end
     end
   end
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -2,18 +2,18 @@ require 'helper'
 
 describe TryRb::CLI do
   describe '::start' do
-    around :each do
-      TryRb::CLI.editor = "mvim"
-    end
-    it "should call system method with an EDITOR" do
-      TryRb::CLI.editor = "fav_editor"
-      expect(TryRb::CLI).to receive(:system).with('fav_editor', 'filepath')
-      TryRb::CLI.start(["filepath"])
+    it "should call system method with an editor" do
+      TryRb::Config.instance.path = fixture_file_path('tryrbrc')
+      expect(TryRb::CLI).to receive(:system).with('emacs', '~/tmp/foo/tryrb/filename.rb')
+      TryRb::CLI.start(["filename"])
     end
 
     it "should call system method with right filepath and default editor" do
-      expect(TryRb::CLI).to receive(:system).with('mvim', 'filepath')
-      TryRb::CLI.start(["filepath"])
+      TryRb::Config.instance.path = fixture_file_path('foorc')
+      expect(TryRb::CLI).to receive(:system).with('mvim', '~/tmp/tryrb/filename.rb')
+      TryRb::CLI.start(["filename"])
+    end
+  end
     end
   end
 end
